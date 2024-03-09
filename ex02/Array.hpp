386ef3ddff62541122807612527c9f7cac931867
cquintana001 <cquintana002@gmail.com>
2024-03-09 19:34:21 +0100

    final commit

---
+++ ex02/Array.hpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+template < typename T >
+class Array {
+
+private:
+    T*              _array;
+    unsigned int    _size;
+
+public:
+    Array() : _array( new T() ), _size( 0 ) {};
+    Array( unsigned int n ) : _array( new T[n] ), _size( n ) {};
+    Array( const Array& rhs ) : _array( new T[rhs.size()] ), _size( rhs.size() ) {
+        for ( unsigned int i( 0 ); i < _size; i++ )
+            _array[i] = rhs._array[i];
+    };
+
+    Array& operator=( const Array& rhs ) {
+        if ( this != &rhs ) {
+            delete [] _array;
+            _array = new T[rhs.size()];
+            _size = rhs._size;
+            for ( unsigned int i( 0 ); i < _size; i++ )
+                _array[i] = rhs._array[i];
+        }
+        return *this;
+    }
+
+    T& operator[]( unsigned int i ) const {
+        if ( i >= _size )
+            throw outOfLimits();
+        return _array[i];
+    }
+
+    unsigned int  size( void ) const { return _size; }
+
+    ~Array( void ) { delete [] _array; }
+
+    class outOfLimits : public std::exception {
+        public:
+            virtual const char* what() const throw() { return "Index is out of limits";}
+    };
+};
+
+template < typename T >
+std::ostream& operator<<( std::ostream& out, const Array<T>& arr ) {
+    for ( unsigned int i( 0 ); i < arr.size(); i++ )
+        out << arr[i] << " ";
+    return out;
+}
